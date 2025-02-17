3432a818a762c3e28dd6b5d798e6d5a0e9c38fca
AdvayAtMason <advay.chandra27@masonohioschools.com>
2025-02-17 13:03:11 -0500

    Fixed routing errors. Project should run fine.

--- app.py
+++ app.py
@@ -5,9 +5,17 @@ app = Flask(__name__)
 
 #This is how we will route people to the webpage. To route to a new webpage, simply make a new version of this.
 #We can also use these functions to make custom methods. Look up the flask documentation for how to pass variables in.
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+@app.route("/contact", methods=["GET"])
+def contact():
+    return render_template("contact.html")
 
 #This is is the program to run the server on a Localhost PC.
 #We recommend that you run this on a virtual machine (NOTE: All VENV Folders are ignored) on Windows 10 or newer
